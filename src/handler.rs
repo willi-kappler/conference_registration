0c67b20ba9bc0ce5cdcb60343a0b9b72c5887c83
Willi Kappler <willi.kappler@uni-tuebingen.de>
2016-10-06 18:03:50 +0200

    start processing of form data

--- src/handler.rs
+++ src/handler.rs
@@ -7,10 +7,83 @@ use handlebars_iron::{Template};
 
 use rustc_serialize::json::Json;
 
+use params::{Params, Value, Map};
+
+use plugin::Pluggable;
+
+
+enum PriceCategory {
+    Student,
+    Regular
+}
+
+struct Registration {
+    last_name: String,
+    first_name: String,
+    institutuion: String,
+    street: String,
+    street_no: String,
+    zip_code: String,
+    city: String,
+    phone: String,
+    e_mail: String,
+    more_info: String,
+    price_category: PriceCategory
+}
+
+
 pub fn handle_main(req: &mut Request) -> IronResult<Response> {
+    let map = req.get_ref::<Params>().unwrap();
+    
     let mut resp = Response::new();
 
+    info!("handle_main: {:?}", map);
+    
     let mut data : BTreeMap<String, Json> = BTreeMap::new();
     resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
     Ok(resp)
+}
+
+pub fn handle_submit(req: &mut Request) -> IronResult<Response> {
+    let map = req.get_ref::<Params>().unwrap();
+    
+    let mut resp = Response::new();
+
+    info!("handle_submit: {:?}", map);
+
+    match map2Registration(map) {
+        Some(registration) => insert_to_db(registration),
+        None => info!("Registration invalid...")
+    }
+    
+    let mut data : BTreeMap<String, Json> = BTreeMap::new();
+    resp.set_mut(Template::new("submit", data)).set_mut(status::Ok);
+    Ok(resp)
+}
+
+fn map2Registration(map: &Map) -> Option<Registration> {
+    let mut result = Registration{
+        last_name: "".to_string(),
+        first_name: "".to_string(),
+        institutuion: "".to_string(),
+        street: "".to_string(),
+        street_no: "".to_string(),
+        zip_code: "".to_string(),
+        city: "".to_string(),
+        phone: "".to_string(),
+        e_mail: "".to_string(),
+        more_info: "".to_string(),
+        price_category: PriceCategory::Student,
+    };
+    
+    match map.find(&["last_name"]) {
+        Some(&Value::String(ref last_name)) => result.last_name = last_name.to_string(),
+        _ => return None
+    }
+    
+    Some(result)
+}
+
+fn insert_to_db(registration: Registration) {
+    // TODO
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ extern crate staticfile;
 extern crate rusqlite;
 extern crate handlebars_iron;
 extern crate rustc_serialize;
+extern crate params;
+extern crate plugin;
+#[macro_use] extern crate log;
+extern crate simplelog;
 
 // System modules
 
 use std::error::Error;
 use std::path::Path;
+use std::fs::File;
 
 // External modules
 
@@ -20,6 +25,7 @@ use mount::Mount;
 use staticfile::Static;
 use rusqlite::{SqliteConnection};
 use handlebars_iron::{HandlebarsEngine, DirectorySource};
+use simplelog::{FileLogger, LogLevelFilter};
 
 // Local modules
 
@@ -29,13 +35,15 @@ mod handler;
 
 
 use config::load_configuration;
-use handler::handle_main;
+use handler::{handle_main, handle_submit};
 
 fn main() {
 
     // TODO: look at persistent:
     // https://github.com/iron/persistent/blob/master/examples/hitcounter.rs
 
+    let _ = FileLogger::init(LogLevelFilter::Info, File::create("registration.log").unwrap());
+    
     let config = load_configuration("");
 
     let db_conn = SqliteConnection::open(config.db_filename).unwrap();
@@ -51,6 +59,9 @@ fn main() {
 
     router.get("/", handle_main, "index");
     router.post("/", handle_main, "index");
+
+    router.get("/submit", handle_submit, "submit");
+    router.post("/submit", handle_submit, "submit");
 
     let mut mount = Mount::new();
 
